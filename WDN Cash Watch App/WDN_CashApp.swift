f9fa0089f0aa328937be300b66e4815df2721197
Luan9806 <luanlongboard@hotmail.com>
2025-05-12 20:40:38 -0300

    Initial Commit

---
+++ WDN Cash Watch App/WDN_CashApp.swift
@@ -0,0 +1,17 @@
+//
+//  WDN_CashApp.swift
+//  WDN Cash Watch App
+//
+//  Created by Luan Carlos on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WDN_Cash_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WDN Cash Watch AppTests/WDN_Cash_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WDN_Cash_Watch_AppTests.swift
+//  WDN Cash Watch AppTests
+//
+//  Created by Luan Carlos on 12/05/25.
+//
+
+import Testing
+@testable import WDN_Cash_Watch_App
+
+struct WDN_Cash_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
